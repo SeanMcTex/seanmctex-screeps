33810288c33d1332390a43a01604b4323abf71e1
Sean McMains <sean@mcmains.net>
2018-06-27 06:49:53 -0500

    Basic repair logic. Have harvesters say what they're up to. This will soon be replaced by a state machine for the harvesters.

--- dist/role.harvester.js
+++ dist/role.harvester.js
@@ -24,11 +24,29 @@ var structuresNeedingEnergy = (creep) => {
 	});
 }
 
+var structuresNeedingRepair = (creep) => {
+	return creep.room.find(FIND_STRUCTURES, {
+		filter: (structure) => {
+			return  structure.hits < structure.hitsMax;
+		}
+	});
+}
+
 var giveEnergyTo = (creep, structure) => {
 	if (creep.transfer(structure, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
 		creep.moveTo(structure, {
 			visualizePathStyle: {
 				stroke: '#ffffff'
+			}
+		});
+	}
+}
+
+var goRepair = (creep, structure) => {
+	if (creep.repair(structure) == ERR_NOT_IN_RANGE) {
+		creep.moveTo(structure, {
+			visualizePathStyle: {
+				stroke: '#ff0000'
 			}
 		});
 	}
@@ -53,26 +71,43 @@ var roleHarvester = {
 
 	/** @param {Creep} creep **/
 	run: function(creep) {
+		creep.say('RUN');
 		if (creep.carry.energy < creep.carryCapacity && !creep.memory.building) {
+			creep.say('HFS');
 			harvestFromSource(creep);
 		} else {
 			var targets = structuresNeedingEnergy(creep);
-			if (targets.length > 0) {
+			if (targets.length > 0 && creep.carry.energy > 0) {
+				creep.say('GET');
 				giveEnergyTo(creep, targets[0]);
 			} else {
+				var structures = structuresNeedingRepair(creep)
+				if (structures.length > 0) {
+					creep.say('GR');
+					
+					goRepair(creep,structures[0]);
+				}
 				if (creep.memory.building && creep.carry.energy == 0) {
+					creep.say('FB');
+					
 					finishedBuilding(creep);
 				}
 				if (!creep.memory.building && creep.carry.energy == creep.carryCapacity) {
+					creep.say('SBM');
 					setBuildMode(creep);
 				}
 
 				if (creep.memory.building) {
 					var targets = creep.room.find(FIND_CONSTRUCTION_SITES);
 					if (targets.length) {
+						creep.say('BAS');
+						
 						buildAtSite(creep, targets[0]);
 					}
+					
 				} else {
+					creep.say('HFS');
+					
 					harvestFromSource(creep);
 				}
 			}
